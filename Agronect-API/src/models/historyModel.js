3c9c8fbd3ee1f6256e7feb887b637e0652be37c4
alhailrose <sfqbgs77@gmail.com>
2024-06-21 01:04:43 +0700

    edit

--- Agronect-API/src/models/historyModel.js
+++ Agronect-API/src/models/historyModel.js
@@ -4,9 +4,12 @@ import dbPool from "../config/connection.js";
 const getAllHistory = async () => {
     try {
         const [rows] = await dbPool.execute(
-            "SELECT id_pred, prediction, confidence, description, solution, user_id,plant_name,image FROM predictions ORDER BY created_at DESC"
+            "SELECT id_pred, prediction, confidence, description, solution, user_id, plant_name, image FROM predictions ORDER BY created_at DESC"
         );
-        return rows;
+        return rows.map((row) => ({
+            ...row,
+            image: row.image ? Buffer.from(row.image).toString("base64") : null,
+        }));
     } catch (error) {
         throw error;
     }
@@ -27,7 +30,7 @@ const getHistoryByIdModel = async (id_pred) => {
 const getAllHistoryByUserIdModel = async (user_id) => {
     try {
         const [rows] = await dbPool.execute(
-            "SELECT id_pred, prediction, confidence, description, solution, user_id FROM predictions WHERE user_id = ? ORDER BY created_at DESC",
+            "SELECT id_pred, prediction, confidence, description, solution, user_id,image FROM predictions WHERE user_id = ? ORDER BY created_at DESC",
             [user_id]
         );
         return rows;
